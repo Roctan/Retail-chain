7bcad648d09e5578f7d145b6fd69bb414cb65265
Rohit Tandon <rohitandon24@gmail.com>
2022-11-26 23:02:00 +0530

    Create retail-chain.sol

---
+++ retail-chain.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+contract retail{
+struct Item{
+    string name;
+    uint item_no;
+    uint price;
+    uint quantity;
+    address wallet;
+}
+
+    uint public items = 1;
+    address owner = msg.sender;
+    mapping(uint=>Item) public item;
+   
+    function record_item(string memory _name, uint itemnum, uint _price, uint _quant, address _add) public {
+      item[items] = Item({
+            name: _name,
+            item_no: itemnum,
+            price: _price,
+            quantity: _quant,
+            wallet: _add
+        });
+        items++;
+    }
+    function check(uint256 orderno,string memory _name, uint itemnum, uint _price, uint _quant,address _add) external view returns(string memory){
+        require(owner==msg.sender, "items maybe corrupted because sender is not same!");
+        require(item[orderno].wallet==_add,"The address of reciever is lost!");
+        require(keccak256(abi.encodePacked((item[orderno].name))) == keccak256(abi.encodePacked((_name))),"The name does not match!");
+        require(item[orderno].item_no == itemnum,"Item number does not match!");
+        require(item[orderno].price == _price,"The price of the item does not match");
+        require(item[orderno].quantity == _quant,"The quantity of the item does not match!");
+        return "The item is correct and ready to go!";
+    }
+}
